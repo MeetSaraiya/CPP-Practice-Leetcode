0f0f62869c2699f38cb0562d4802eea42e846ff2
Meet Saraiya <127649875+MeetSaraiya@users.noreply.github.com>
2025-02-13 14:27:51 +0530

    correct code

--- 3036-min-op-to-exceed-k.cpp
+++ 3036-min-op-to-exceed-k.cpp
@@ -1,27 +1,50 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int minOperations(vector<int>& nums, int k) {
+//     set<int>s;
+//     int c = 0;
+//     for(int i:nums)
+//         s.insert(i);
+
+//     while(s.size() > 2 && *s.begin()<k){
+//     int x = *s.begin();
+//     s.erase(x);
+//     int y = *s.begin();
+//     s.erase(y);
+//     int ans = min(x, y) * 2 + max(x, y);
+//     s.insert(ans);
+//     c++;
+//     }
+//     return c;
+// }
+// int main(){
+//     vector<int>nums = {2,11,10,1,3};
+//     int k = 10;
+//     cout<<minOperations(nums,k);
+
+
+    
+// }
 #include<bits/stdc++.h>
 using namespace std;
 int minOperations(vector<int>& nums, int k) {
-    set<int>s;
+    priority_queue<int,vector<int>,greater<int>>s(nums.begin(),nums.end());
     int c = 0;
-    for(int i:nums)
-        s.insert(i);
 
-    while(s.size() > 2 && *s.begin()<k){
-    int x = *s.begin();
-    s.erase(x);
-    int y = *s.begin();
-    s.erase(y);
-    int ans = min(x, y) * 2 + max(x, y);
-    s.insert(ans);
+    while(s.size() >= 2 && s.top()<k){
+    int x = s.top();//x is smallest element
+    s.pop();
+    int y = s.top();//y is second smallest element
+    s.pop();
+    //int ans = min(x,y) * 2 + max(x,y);  
+    int ans = x * 2 + y; // 
+    s.push(ans);
     c++;
     }
     return c;
 }
 int main(){
     vector<int>nums = {2,11,10,1,3};
-    int k = 10;
+    int k = 20;
     cout<<minOperations(nums,k);
-
-
-    
 }
